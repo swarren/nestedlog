db2795b83534e69d02177df99ef6addbec864578
Stephen Warren <swarren@wwwdotorg.org>
2021-03-17 07:32:01 -0600

    run_as_block: catch exceptions, implement a context manager
    subprocess.run() can throw an exception if the command can't be launched
    at all, e.g. if the specified executable doesn't exist.
    
    This is most easily solved by implementing a full context manager, which
    is also a useful addition to the API.
    
    Fixes #7.
    Fixes #4.

--- lib/python/nestedlog/api.py
+++ lib/python/nestedlog/api.py
@@ -1,6 +1,7 @@
 # Copyright 2021 Stephen Warren <swarren@wwwdotorg.org>
 # SPDX-License-Identifier: MIT
 
+from contextlib import contextmanager
 import os
 import socket
 import subprocess
@@ -20,14 +21,38 @@ def start_block(block_name):
 def end_block(status):
     _send_cmd(f'{CMD_END_BLOCK} {status}\n')
 
+class MarkBlockAsFailedException(Exception):
+    pass
+
+class BlockFailedException(Exception):
+    pass
+
+@contextmanager
+def run_python_as_block(block_name, exit_on_fail=False):
+    start_block(block_name)
+    status = nld.STATUS_AUTO
+    try:
+        try:
+            yield None
+        finally:
+            sys.stdout.flush()
+    except MarkBlockAsFailedException:
+        status = nld.STATUS_ERROR
+    except:
+        print('ERROR: Exception thrown:', file=sys.stderr)
+        import traceback
+        traceback.print_exc(file=sys.stderr)
+        status = nld.STATUS_ERROR
+    end_block(nld.status_to_text[status])
+    if status != nld.STATUS_AUTO:
+        raise BlockFailedException()
+
 def run_as_block(block_name, cmd):
-    _send_cmd(f'{CMD_START_BLOCK} {block_name}\n')
-    cpe = subprocess.run(cmd)
-    if cpe.returncode == 0:
-        status = nld.status_to_text[nld.STATUS_OK]
-    else:
-        status = nld.status_to_text[nld.STATUS_ERROR]
-    _send_cmd(f'{CMD_END_BLOCK} {status}\n')
+    with run_python_as_block(block_name):
+        cpe = subprocess.run(cmd)
+        if cpe.returncode != 0:
+            print('ERROR: Process exit code ' + str(cpe.returncode), file=sys.stderr)
+            raise MarkBlockAsFailedException()
 
 def _send_cmd(cmd):
     sock_path = os.environ[SOCK_ENV_VAR]
